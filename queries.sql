17e223f14a146e6e5d542b848cff5e1530f832d8
Maryvonne ALLARD <150820366+KINOAHng@users.noreply.github.com>
2026-01-22 23:09:24 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,12 +7,12 @@ SHOW SCHEMAS FROM minio;
 -- Liste des tables dans le schéma logs de MinIO
 SHOW TABLES FROM minio.logs;
 
--- Affichage des 10 premières intrusions détectées
-SELECT * FROM minio.logs.predictions 
-WHERE Intrusion = 1 
+-- Affichage des 10 premières intrusions détectées (basé sur la prédiction du modèle)
+SELECT * FROM minio.logs.predictions_parquet
+WHERE prediction = 1.0
 LIMIT 10;
 
--- Comptage du nombre total d'intrusions détectées
-SELECT COUNT(*) AS nombre_intrusions 
-FROM minio.logs.predictions 
-WHERE Intrusion = 1;
+-- Comptage du nombre total d'intrusions détectées (basé sur la prédiction)
+SELECT COUNT(*) AS nombre_intrusions
+FROM minio.logs.predictions_parquet
+WHERE prediction = 1.0;
